263e21f7b6daac2a4b0f5aa80592d927d0147a3c
Manash <manash@cefalo.com>
2019-12-10 10:51:33 +0600

    Clean up & add home page link

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -37,12 +37,6 @@ scratch. This page gets rid of all links and provides the needed markup only.
             <li class="nav-item">
                 <a class="nav-link" data-widget="pushmenu" href="#"><i class="fas fa-bars"></i></a>
             </li>
-{{--            <li class="nav-item d-none d-sm-inline-block">--}}
-{{--                <a href="index3.html" class="nav-link">Home</a>--}}
-{{--            </li>--}}
-{{--            <li class="nav-item d-none d-sm-inline-block">--}}
-{{--                <a href="#" class="nav-link">Contact</a>--}}
-{{--            </li>--}}
         </ul>
 
         <!-- SEARCH FORM -->
@@ -81,7 +75,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
     <!-- Main Sidebar Container -->
     <aside class="main-sidebar sidebar-dark-primary elevation-4">
         <!-- Brand Logo -->
-        <a href="index3.html" class="brand-link">
+        <a href="{{ route('home') }}" class="brand-link">
             <img src="{{ asset('/bower_components/admin-lte/dist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
                  style="opacity: .8">
             <span class="brand-text font-weight-light">Laravel Admin</span>
