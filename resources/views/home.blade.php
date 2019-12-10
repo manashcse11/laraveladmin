3983f07f9d7d0925ea263e048baeb3a31ca254b1
Manash <manash@cefalo.com>
2019-12-10 10:54:22 +0600

    Remove breadcrumb

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,12 +8,12 @@
                 <div class="col-sm-6">
                     <h1 class="m-0 text-dark">Starter Page</h1>
                 </div><!-- /.col -->
-                <div class="col-sm-6">
-                    <ol class="breadcrumb float-sm-right">
-                        <li class="breadcrumb-item"><a href="{{ route('home') }}">Home</a></li>
-                        <li class="breadcrumb-item active">Starter Page</li>
-                    </ol>
-                </div><!-- /.col -->
+{{--                <div class="col-sm-6">--}}
+{{--                    <ol class="breadcrumb float-sm-right">--}}
+{{--                        <li class="breadcrumb-item"><a href="{{ route('home') }}">Home</a></li>--}}
+{{--                        <li class="breadcrumb-item active">Starter Page</li>--}}
+{{--                    </ol>--}}
+{{--                </div><!-- /.col -->--}}
             </div><!-- /.row -->
         </div><!-- /.container-fluid -->
     </div>
--- resources/views/user/create.blade.php
+++ resources/views/user/create.blade.php
@@ -8,12 +8,12 @@
                 <div class="col-sm-6">
                     <h1 class="m-0 text-dark">User Create</h1>
                 </div><!-- /.col -->
-                <div class="col-sm-6">
-                    <ol class="breadcrumb float-sm-right">
-                        <li class="breadcrumb-item"><a href="#">Home</a></li>
-                        <li class="breadcrumb-item active">User Create</li>
-                    </ol>
-                </div><!-- /.col -->
+{{--                <div class="col-sm-6">--}}
+{{--                    <ol class="breadcrumb float-sm-right">--}}
+{{--                        <li class="breadcrumb-item"><a href="#">Home</a></li>--}}
+{{--                        <li class="breadcrumb-item active">User Create</li>--}}
+{{--                    </ol>--}}
+{{--                </div><!-- /.col -->--}}
             </div><!-- /.row -->
         </div><!-- /.container-fluid -->
     </div>
